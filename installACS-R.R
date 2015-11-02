929c1777e01038b2ab2450329e6de4c21dc17352
robnit <rip@umd.edu>
2015-11-02 13:45:49 -0500

    Install script to install the package and the API key. 	new file:   installACS-R.R

---
+++ installACS-R.R
@@ -0,0 +1,8 @@
+#You only need to run this script one time.
+
+#This will install ACS-R and maybe some other packages.  
+#Just follow the prompts as necessary.
+install.packages("acs")
+
+#Enter your Census API Key by replacing the elipses
+api.key.install(key="...")
