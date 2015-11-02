e097e64997fed9c45664a11849ebd1625ded2174
robnit <rip@umd.edu>
2015-11-02 15:23:12 -0500

    Added geographies and measures files

---
+++ geographies.R
@@ -0,0 +1,23 @@
+#make sure ACS library is loaded
+library("acs")
+
+#Create geographic variables for different States.
+
+California = geo.make(state = "CA", county = c("Contra Costa", "Marin", "San Mateo", "Ventura", "Santa Clara"))
+Colorado = geo.make(state = "CO", county = "Douglas")
+DC = geo.make(state = "DC")
+Illinois = geo.make(state = "IL", county = c("DuPage", "Lake"))
+Indiana = geo.make(state = "IN", county = "Hamilton")
+Kansas = geo.make(state = "KS", county = "Johnson")
+Maryland = geo.make(state = "MD", county = c("Anne Arundel", "Baltimore County", "Frederick", "Howard", "Montgomery", "Prince George's County"))
+Michigan = geo.make(state = "MI", county = "Oakland")
+Minnesota = geo.make(state = "MN", county = c("Dakota", "Washington"))
+New_Jersey = geo.make(state = "NJ", county = c("Bergen", "Middlesex", "Monmouth", "Morris", "Somerset"))
+New_York = geo.make(state = "NY", county = c("Nassau", "Rockland", "Suffolk", "Westchester"))
+Pennsylvania = geo.make(state = "PA", county = c("Bucks", "Chester", "Montgomery"))
+Texas = geo.make(state = "TX", county = c("Collin County", "Fort Bend"))
+Virginia = geo.make(state = "VA", county = c("Arlington County", "Fairfax County", "Loudoun County", "Prince William County"))
+Wisconsin = geo.make(state = "WI", county = "Waukesha")
+
+allstates = California+Colorado+DC+Illinois+Indiana+Kansas+Maryland+Michigan+Minnesota+New_Jersey+New_York+Pennsylvania+Texas+Virginia+Wisconsin
+
---
+++ measures.R
@@ -0,0 +1,13 @@
+#Table numbers assigned to names
+
+homeownership = "B25003"
+housingburden_owners = "B25101"
+housingburden_renters = "B25106"
+medianvalueofhousing_owner = "B25077"
+median_gross_rent = "B25111"
+commute_time = "B08136"
+percent_public_transit = "B08136" #use keyword "public"
+below_poverty_line = "B17001"
+median_household_income = "B19013"
+in_labor_force = "B23025"
+
